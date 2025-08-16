5613c752e8bcfb79bbb7904932214dc6c15cab68
invincy <vaibhav.damor@gmail.com>
2025-08-16 05:36:24 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -877,8 +877,7 @@
 
             showToast('💾 Progress saved successfully!');
 
-            alert('💾 Progress saved successfully!');
-
+        
             deathClaimForm.classList.add('hidden');
             resetForm();
         });
@@ -943,3 +942,4 @@
         }
 
 (function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'96fbbb47543f45ed',t:'MTc1NTI5Mjc4MS4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();
+
